9fdb3571a11b2187988cc616c8fef991d0b7cfbf
Scott Rice <scottrice10@gmail.com>
2015-03-27 15:40:16 -0700

    Implementing operators solution and tests

--- spec/implementingOperatorsSpec.js
+++ spec/implementingOperatorsSpec.js
@@ -1,5 +1,13 @@
 describe("implementingOperators", function() {
-  it("should be implemented", function() {
-    expect(false).to.be.equal(true);
+  it("should multiply integers", function() {
+    expect(multiply(2,2)).to.be.equal(4);
+  });
+
+  it("should divide integers", function() {
+    expect(divide(4,2)).to.be.equal(2);
+  });
+
+  it("should find modulus of integers", function() {
+    expect(modulo(5,3)).to.be.equal(2);
   });
 });
