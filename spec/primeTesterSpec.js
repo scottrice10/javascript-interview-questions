389235ed4759c1bcbf5671994f9dadeb9e8e7f3b
Scott Rice <scottrice10@gmail.com>
2015-04-01 15:16:14 -0700

    Prime test solution and tests

--- spec/primeTesterSpec.js
+++ spec/primeTesterSpec.js
@@ -1,5 +1,19 @@
 describe("primeTester", function() {
-  it("should be implemented", function() {
-    expect(false).to.be.equal(true);
+  it("should return true for 11", function() {
+    expect(primeTester(11)).to.be.equal(true);
+  });
+
+  it("should false for 100", function() {
+    expect(primeTester(100)).to.be.equal(false);
+  });
+});
+
+describe("primeSieve", function() {
+  it("should find all prime numbers in the range 0 to 100", function() {
+    expect(primeSieve(0,100).length).to.be.equal(25);
+  });
+
+  it("should find all prime numbers in the range 1000 to 2000", function() {
+    expect(primeSieve(1000,2000).length).to.be.equal(135);
   });
 });
