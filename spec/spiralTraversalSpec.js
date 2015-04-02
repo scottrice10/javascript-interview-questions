1bb9e70b4bbe64c0b15227427804cbb419c438be
Scott Rice <scottrice10@gmail.com>
2015-04-01 21:56:03 -0700

    Spiral traversal solution and tests

--- spec/spiralTraversalSpec.js
+++ spec/spiralTraversalSpec.js
@@ -1,5 +1,13 @@
 describe("spiralTraversal", function() {
   it("should be implemented", function() {
-    expect(false).to.be.equal(true);
+    var array = spiralTraversal([
+      [1,2,3],
+      [4,5,6],
+      [7,8,9]
+    ]); // [1, 2, 3, 6, 9, 8, 7, 4, 5]
+
+    expect(array.length).to.be.equal(9);
+    expect(array[3]).to.be.equal(6);
+    expect(array[7]).to.be.equal(4);
   });
 });
--- spiralTraversal/spiralTraversal.js
+++ spiralTraversal/spiralTraversal.js
@@ -14,7 +14,30 @@
     returns [1, 2, 3, 6, 9, 8, 7, 4, 5]
  */
 
-var spiralTraversal = function(matrix){
+var spiralTraversal = function(matrix, result){
+  result = result || [];
 
-  // TODO: Implement me!
+  if(matrix.length === 0){
+    return result;
+  }
+
+  // top, move left to right
+  result = result.concat(matrix.shift());
+
+  //right, move top to bottom
+  for(var i=0;i<matrix.length;i++){
+    result = result.concat(matrix[i].splice(matrix[i].length - 1, 1));
+  }
+
+  //bottom, move right to left
+  if(matrix.length > 0){
+    result = result.concat(matrix.pop().reverse());
+  }
+
+  //left, move bottom to top
+  for(var j=matrix.length - 1;j>=0;j--){
+    result = result.concat(matrix[j].splice(0, 1));
+  }
+
+  return spiralTraversal(matrix, result)
 };
