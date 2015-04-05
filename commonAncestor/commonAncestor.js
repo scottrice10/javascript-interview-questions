ba6fd491aab06aeb3b0957e2ad73f614f0441971
Scott Rice <scottrice10@gmail.com>
2015-04-04 17:45:36 -0700

    Common ancestor

--- commonAncestor/commonAncestor.js
+++ commonAncestor/commonAncestor.js
@@ -12,17 +12,18 @@
  * grandma.getAncestorPath(me); // => [grandma, mom, me]
  */
 
-var Tree = function(){
+var Tree = commonAncestor = function(value) {
   this.children = [];
+  this.value = value || null;
 };
 
 /**
  * add an immediate child
  */
-Tree.prototype.addChild = function(child){
-  if(!this.isDescendant(child)){
+Tree.prototype.addChild = function(child) {
+  if(!this.isDescendant(child)) {
     this.children.push(child);
-  }else {
+  } else {
     throw new Error("That child is already a child of this tree");
   }
   return this;
@@ -37,9 +38,22 @@ Tree.prototype.addChild = function(child){
  *  3.) between my grandma and my grandma -> my grandma
  *  4.) between me and a potato -> null
  */
-Tree.prototype.getClosestCommonAncestor = function(/*...*/
-){
-  // TODO: implement me!
+Tree.prototype.getClosestCommonAncestor = function(root1, root2) {
+  var result = null;
+  var root1Path = this.getAncestorPath(root1);
+  var root2Path = this.getAncestorPath(root2);
+
+  if(!root1Path || !root2Path) {
+    return null;
+  }
+
+  for(var i = 0; i < root1Path.length; i++) {
+    if(root1Path[i] === root2Path[i]){
+      result = root1Path[i];
+    }
+  }
+
+  return result;
 };
 
 /**
@@ -50,22 +64,35 @@ Tree.prototype.getClosestCommonAncestor = function(/*...*/
  * 3.) me.getAncestorPath(me) -> [me]
  * 4.) grandma.getAncestorPath(H R Giger) -> null
  */
-Tree.prototype.getAncestorPath = function(/*...*/
-){
-  // TODO: implement me!
-}
+Tree.prototype.getAncestorPath = function(targetNode) {
+  var result = null;
+
+  var recurse = function(currNode, currPath) {
+    if(targetNode.value === currNode.value) {
+      return result = currPath;
+    }
+
+    currNode.children.forEach(function(child) {
+      recurse(child, currPath.concat(child.value));
+    });
+  };
+
+  recurse(this, [this.value]);
+
+  return result;
+};
 
 /**
  * check to see if the provided tree is already a child of this
  * tree __or any of its sub trees__
  */
-Tree.prototype.isDescendant = function(child){
-  if(this.children.indexOf(child) !== -1){
+Tree.prototype.isDescendant = function(child) {
+  if(this.children.indexOf(child) !== -1) {
     // `child` is an immediate child of this tree
     return true;
-  }else{
-    for(var i = 0; i < this.children.length; i++){
-      if(this.children[i].isDescendant(child)){
+  } else {
+    for(var i = 0; i < this.children.length; i++) {
+      if(this.children[i].isDescendant(child)) {
         // `child` is descendant of this tree
         return true;
       }
@@ -77,12 +104,12 @@ Tree.prototype.isDescendant = function(child){
 /**
  * remove an immediate child
  */
-Tree.prototype.removeChild = function(child){
+Tree.prototype.removeChild = function(child) {
   var index = this.children.indexOf(child);
-  if(index !== -1){
+  if(index !== -1) {
     // remove the child
-    this.children.splice(index,1);
-  }else{
+    this.children.splice(index, 1);
+  } else {
     throw new Error("That node is not an immediate child of this tree");
   }
 };
