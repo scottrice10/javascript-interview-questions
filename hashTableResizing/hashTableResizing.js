96efa4efdda22be9716bd4e1303783ead3987a71
Scott Rice <scottrice10@gmail.com>
2015-04-06 02:14:01 +0000

    Hashtable resizing

--- hashTableResizing/hashTableResizing.js
+++ hashTableResizing/hashTableResizing.js
@@ -8,6 +8,95 @@
  */
 
 var hashtableResizing = function() {
+   var obj = {};
+   var _size = 4;
+   var _storage = [];
+   
+   obj.insert = function(key,val){
+      var i = getIndexBelowMaxForKey(key,_size);
+      
+      if(_storage[i]){
+         var bucket = _storage[i];
+         var keyExists = false;
+         var itemAdded = false;
+         
+         for(var j=0;j<bucket.length;j++){
+            if(bucket[j] && bucket[j][0] === key){
+               keyExists = true;
+               bucket[j][1] = val;
+               break;
+            }
+         }
+         
+         if(!keyExists){
+            itemAdded = true;
+            _storage[i].push([key,val]);
+         }
+      } else {
+         itemAdded = true;
+         _storage[i] = [[key,val]];
+      }
+      
+       if(itemAdded && _storage.length > _size * 0.75){
+         resize(_size * 2);
+      }
+   }
+   
+   obj.retrieve = function(key){
+      var i = getIndexBelowMaxForKey(key, _size);
+      
+      if(_storage[i]){
+         for(var j=0;j<_storage[i].length;j++){
+            if(_storage[i][j] && _storage[i][j][0] === key){
+               return _storage[i][j][1];
+            }
+         }
+      }
+      
+      return null;
+   }
+   
+   obj.remove = function(key){
+      var i = getIndexBelowMaxForKey(key, _size);
+      var itemRemoved = false;
+      
+      if(_storage[i]){
+         for(var j=0;j<_storage[i].length;j++){
+            if(_storage[i][j] && _storage[i][j][0] === key){
+               itemRemoved = true;
+               delete _storage[i][j];
+            }
+         }
+      }
+      
+      if(itemRemoved && _storage.length < _size * 0.25){
+         resize(_size / 2);
+      }
+   }
+   
+   function resize(newSize){
+      var tempStorage = [];
+      
+      for(var i=0;i<_storage.length;i++){
+         var bucket = _storage[i];
+         if(bucket){
+            for(var j=0;j<bucket.length;j++){
+               var tuple = bucket[j];
+               
+               tempStorage.push([tuple[0],tuple[1]])
+            }
+         }
+      }
+      
+      _storage = [];
+      _size = newSize;
+      
+      for(var k=0;k<tempStorage.length;k++){
+         obj.insert(tempStorage[k][0], tempStorage[k][1]);
+      }
+   }
+   
+   return obj;
 
 };
 
