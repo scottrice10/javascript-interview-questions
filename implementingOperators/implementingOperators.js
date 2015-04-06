e87db6aea3064e660f3da4077e456b9285a6cb1d
Scott Rice <scottrice10@gmail.com>
2015-04-06 10:54:18 -0700

    Implementing operators

--- implementingOperators/implementingOperators.js
+++ implementingOperators/implementingOperators.js
@@ -11,15 +11,67 @@
 
 
 var multiply = function(x, y) {
+   var product = 0;
+   var isNegative = false;
 
+   if(x < 0 || y < 0 && !(x < 0 && y < 0)){
+      isNegative = true;
+   }
+
+   x = Math.abs(x);
+   y = Math.abs(y);
+
+   var recurse = function(currProd, multiplier){
+      if(multiplier === y){
+         return product = currProd;
+      }
+
+      recurse(currProd + x, multiplier + 1);
+   };
+
+   recurse(product, 0);
+
+   if(isNegative){
+      product = 0 - product;
+   }
+
+   return product;
 };
 
 var divide = function(x, y) {
+   var quotient = 1;
+   var isNegative = false;
 
+   if(x < 0 || y < 0 && !(x < 0 && y < 0)){
+      isNegative = true;
+   }
+
+   x = Math.abs(x);
+   y = Math.abs(y);
+
+   var recurse = function(currQuot){
+      if(multiply(currQuot,y) === x){
+         return quotient = currQuot;
+      } else if(multiply(currQuot,y) > x){
+         return quotient = currQuot - 1;
+      }
+
+      recurse(currQuot + 1);
+   };
+
+   recurse(quotient);
+
+   if(isNegative){
+      quotient = 0 - quotient;
+   }
+
+   return quotient;
 };
 
 var modulo = function(x, y) {
+   var quotient = divide(x,y);
 
+   return x - multiply(quotient,y);
 };
 
 
