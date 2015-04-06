4a27ea977d749f4f1c4cf72337e4d028b5222037
Scott Rice <scottrice10@gmail.com>
2015-04-06 15:24:10 -0700

    Longest palindrome

--- longestPalindrome/longestPalindrome.js
+++ longestPalindrome/longestPalindrome.js
@@ -7,5 +7,43 @@
  */
 
 var longestPalindrome = function(string) {
+   var result = "";
 
+   for(var i = 0; i < string.length; i++) {
+      var evenLongest = findEvenLongestPalindrome(string, i);
+      var oddLongest = findOddLongestPalindrome(string, i);
+
+      if(evenLongest.length > result.length) {
+         result = evenLongest;
+      }
+
+      if(oddLongest.length > result.length) {
+         result = oddLongest;
+      }
+   }
+
+   return result;
+};
+
+var findOddLongestPalindrome = function(string, index) {
+   var left = index;
+   var right = index;
+
+   while(string[left] === string[right]) {
+      right++;
+      left--;
+   }
+
+   return string.slice(left + 1, right);
+};
+
+var findEvenLongestPalindrome = function(string, index) {
+   var left = index;
+   var right = index;
+
+   while(string[left] === string[right]) {
+      right++;
+   }
+
+   return string.slice(left, right);
 };
