f17c312936e8ce2af7ae5817d8a69215e0158674
Scott Rice <scottrice10@gmail.com>
2015-04-06 14:44:45 -0700

    Linked list

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -18,4 +18,53 @@
 
 
 var LinkedList = function() {
+   this.head = makeNode();
+   this.tail = makeNode();
 };
+
+LinkedList.prototype.addToTail = function(val){
+   // if no nodes yet in linked list, set head
+   if(!this.head.value){
+      this.head = makeNode(val);
+      this.tail = this.head;
+   } else {
+      // else, iterate through nodes until next is empty, then set next to point to tail
+      var node = this.head;
+      while(node.next){
+         node = node.next;
+      }
+
+      this.tail = makeNode(val);
+      node.next = this.tail;
+   }
+};
+
+LinkedList.prototype.removeHead = function(){
+   if(!this.head.value){
+      return null;
+   }
+
+   var temp = this.head;
+   this.head = this.head.next;
+   return temp.value;
+};
+
+LinkedList.prototype.contains = function(target){
+   var node = this.head;
+   while(node.next){
+      if(node.next.value === target){
+         return true;
+      }
+
+      node = node.next;
+   }
+
+   return false;
+};
+
+function makeNode(val){
+   return {
+      next: null,
+      value: val || null
+   };
+}
