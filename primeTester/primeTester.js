936d23f7303194f6a58abb01dd00c37313250656
Scott Rice <scottrice10@gmail.com>
2015-04-07 11:32:51 -0700

    Prime tester

--- primeTester/primeTester.js
+++ primeTester/primeTester.js
@@ -5,7 +5,13 @@
  */
 
 var primeTester = function(n) {
+   for(var i=2;i<=Math.sqrt(n);i++){
+      if(n % i === 0){
+         return false;
+      }
+   }
 
+   return true;
 };
 
 /* Extra credit: Write a function that generates a list of all prime numbers
@@ -15,7 +21,49 @@ var primeTester = function(n) {
  */
 
 var primeSieve = function (start, end) {
+   var flags = {};
+   var prime = 2;
+   var primeArray = [];
 
+   if(start <= 2){
+      flags[2] = true;
+   }
+
+   for(var j=3;j<=end;j+=2){
+      flags[j] = true;
+   }
+
+   while(prime <= end){
+      flags = checkOffFlags(prime,end,flags);
+
+      prime = getNextPrime(prime,end,flags);
+   }
+
+   for(var i=start;i<=end;i++){
+      if(flags[i]){
+         primeArray.push(i);
+      }
+   }
+
+   return primeArray;
 };
+
+function getNextPrime(prime,end,flags){
+   var next = prime + 1;
+
+   while(!flags[next] && next <= end){
+      next++;
+   }
+
+   return next;
+}
+
+function checkOffFlags(prime,end,flags){
+   for(var l=prime*prime;l<=end;l+=prime){
+      flags[l] = false;
+   }
+
+   return flags;
+}
 
 
