3c27485151c57cf60597a1f9ae74f43c088f85de
Scott Rice <scottrice10@gmail.com>
2015-04-07 21:01:45 -0700

    RomanNumeralTranslator

--- romanNumeralTranslator/romanNumeralTranslator.js
+++ romanNumeralTranslator/romanNumeralTranslator.js
@@ -16,15 +16,32 @@
  */
 
 var DIGIT_VALUES = {
-  I: 1,
-  V: 5,
-  X: 10,
-  L: 50,
-  C: 100,
-  D: 500,
-  M: 1000
+   I: 1,
+   V: 5,
+   X: 10,
+   L: 50,
+   C: 100,
+   D: 500,
+   M: 1000
 };
 
 var translateRomanNumeral = function(romanNumeral) {
+   var skipNext = false;
+   var result = 0;
 
+   for(var i = 0; i < romanNumeral.length; i++) {
+      if(skipNext){
+         skipNext = false;
+         continue;
+      }
+
+      if(i !== romanNumeral.length - 1 && DIGIT_VALUES[romanNumeral[i]] < DIGIT_VALUES[romanNumeral[i + 1]]) {
+         skipNext = true;
+         result += DIGIT_VALUES[romanNumeral[i + 1]] - DIGIT_VALUES[romanNumeral[i]];
+      } else {
+         result += DIGIT_VALUES[romanNumeral[i]];
+      }
+   }
+
+   return result;
 };
