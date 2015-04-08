2eab628109b77c34a3639c3e2535271cab316872
Scott Rice <scottrice10@gmail.com>
2015-04-07 23:48:40 -0700

    SpiralTraversal

--- spiralTraversal/spiralTraversal.js
+++ spiralTraversal/spiralTraversal.js
@@ -5,15 +5,45 @@
  *
  * example:
 
-    spiralTraversal([
-      [1,2,3],
-      [4,5,6],
-      [7,8,9]
-    ]);
+ spiralTraversal([
+ [1,2,3],
+ [4,5,6],
+ [7,8,9]
+ ]);
 
-    returns [1, 2, 3, 6, 9, 8, 7, 4, 5]
+ returns [1, 2, 3, 6, 9, 8, 7, 4, 5]
  */
 
-var spiralTraversal = function(matrix, result){
+var spiralTraversal = function(matrix, result) {
+   result = result || [];
 
+   if(matrix.length === 0) {
+      return result;
+   }
+
+   // top
+   var top = matrix.splice(0, 1);
+   if(top.length > 0){
+      result = result.concat(top[0]);
+   }
+
+   // right
+   for(var i = 0; i < matrix.length; i++) {
+      var right = matrix[i].splice([matrix[i].length - 1],1);
+      result.push(right[0]);
+   }
+
+   //bottom
+   var bottom = matrix.splice(matrix.length - 1, 1);
+   if(bottom.length > 0){
+      result = result.concat(bottom[0].reverse());
+   }
+
+   //left
+   for(var j = matrix.length - 1; j >= 0; j--) {
+      var left = matrix[j].splice(0,1);
+      result.push(left[0]);
+   }
+
+   return spiralTraversal(matrix, result);
 };
