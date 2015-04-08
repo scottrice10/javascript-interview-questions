b85235f41e87937cebceeed3e0b51f034f9940a0
Scott Rice <scottrice10@gmail.com>
2015-04-07 20:28:33 -0700

    RobotPaths

--- robotPaths/robotPaths.js
+++ robotPaths/robotPaths.js
@@ -12,10 +12,46 @@
 // A Board class will be useful
 
 var makeBoard = function(n) {
+   var board = {};
+   board.pieces = [];
 
+   for(var i = 0; i < n; i++) {
+      board.pieces[i] = [];
+      for(var j = 0; j < n; j++) {
+         board.pieces[i][j] = false;
+      }
+   }
+
+   board.togglePiece = function(i, j) {
+      board.pieces[i][j] = !board.pieces[i][j];
+   };
+
+   board.hasAlreadyVisited = function(i,j){
+      return board.pieces[i][j];
+   };
+
+   return board;
 };
 
 var robotPaths = function(n, board, i, j) {
+   board = board || makeBoard(n);
+   i = i || 0;
+   j = j || 0;
 
+   if(i < 0 || i >= n || j < 0 || j >= n || board.hasAlreadyVisited(i,j)) {
+      return 0;
+   }
+
+   if(i === n - 1 && j === n - 1){
+      return 1;
+   }
+
+   board.togglePiece(i,j);
+
+   var paths = robotPaths(n, board, i + 1, j) + robotPaths(n, board, i, j + 1) + robotPaths(n, board, i - 1, j) + robotPaths(n, board, i, j - 1);
+
+   board.togglePiece(i,j);
+
+   return paths;
 };
 
