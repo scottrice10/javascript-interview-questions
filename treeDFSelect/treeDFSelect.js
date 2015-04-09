9cb4e4999e85f01a1e0fe37962bcd571a806172e
Scott Rice <scottrice10@gmail.com>
2015-04-08 21:00:57 -0700

    TreeDFSelect

--- treeDFSelect/treeDFSelect.js
+++ treeDFSelect/treeDFSelect.js
@@ -1,92 +1,111 @@
- /**
-  *
-  * Implement a `DFSelect` method on this Tree class.
-  *
-  * DFSelect accepts a filter function, calls that function on each of the nodes
-  * in Depth First order, and returns a flat array of node values of the tree
-  * for which the filter returns true.
-  *
-  * Example:
-  *   var root1 = new Tree(1);
-  *   var branch2 = root1.addChild(2);
-  *   var branch3 = root1.addChild(3);
-  *   var leaf4 = branch2.addChild(4);
-  *   var leaf5 = branch2.addChild(5);
-  *   var leaf6 = branch3.addChild(6);
-  *   var leaf7 = branch3.addChild(7);
-  *   root1.DFSelect(function (value, depth) {
+/**
+ *
+ * Implement a `DFSelect` method on this Tree class.
+ *
+ * DFSelect accepts a filter function, calls that function on each of the nodes
+ * in Depth First order, and returns a flat array of node values of the tree
+ * for which the filter returns true.
+ *
+ * Example:
+ *   var root1 = new Tree(1);
+ *   var branch2 = root1.addChild(2);
+ *   var branch3 = root1.addChild(3);
+ *   var leaf4 = branch2.addChild(4);
+ *   var leaf5 = branch2.addChild(5);
+ *   var leaf6 = branch3.addChild(6);
+ *   var leaf7 = branch3.addChild(7);
+ *   root1.DFSelect(function (value, depth) {
   *     return value % 2;
   *   })
-  *   // [1, 5, 3, 7]
-  *
-  *   root1.DFSelect(function (value, depth) {
+ *   // [1, 5, 3, 7]
+ *
+ *   root1.DFSelect(function (value, depth) {
   *     return depth === 1;
   *   })
-  *   // [2, 3]
-  *
-  */
+ *   // [2, 3]
+ *
+ */
 
 /*
  * Basic tree that stores a value.
  */
 
-var DFSelect = function(value){
+var DFSelect = function(value) {
+   this.value = value;
+   this.children = [];
+};
+
+DFSelect.prototype.addChild = function(value) {
+   this.children.push(new DFSelect(value));
 };
 
 DFSelect.prototype.DFSelect = function(filter) {
+   var result = [];
+   var recurse = function(obj) {
+      if(filter(obj.node.value, obj.depth)) {
+         result.push(obj.node.value);
+      }
 
+      for(var i = 0; i < obj.node.children.length; i++) {
+         recurse({node: obj.node.children[i], depth: obj.depth + 1});
+      }
+   };
+
+   recurse({node: this, depth: 0});
+
+   return result;
 };
 
 /**
  * You shouldn't need to change anything below here, but feel free to look.
-  */
+ */
 
 /**
-  * add an immediate child
-  * (wrap values in Tree nodes if they're not already)
-  */
-DFSelect.prototype.addChild = function(child){
-  if (!child || !(child instanceof DFSelect)){
-    child = new DFSelect(child);
-  }
+ * add an immediate child
+ * (wrap values in Tree nodes if they're not already)
+ */
+DFSelect.prototype.addChild = function(child) {
+   if(!child || !(child instanceof DFSelect)) {
+      child = new DFSelect(child);
+   }
 
-  if(!this.isDescendant(child)){
-    this.children.push(child);
-  }else {
-    throw new Error("That child is already a child of this tree");
-  }
-  // return the new child node for convenience
-  return child;
+   if(!this.isDescendant(child)) {
+      this.children.push(child);
+   } else {
+      throw new Error("That child is already a child of this tree");
+   }
+   // return the new child node for convenience
+   return child;
 };
 
 /**
-  * check to see if the provided tree is already a child of this
-  * tree __or any of its sub trees__
-  */
-DFSelect.prototype.isDescendant = function(child){
-  if(this.children.indexOf(child) !== -1){
-    // `child` is an immediate child of this tree
-    return true;
-  }else{
-    for(var i = 0; i < this.children.length; i++){
-      if(this.children[i].isDescendant(child)){
-        // `child` is descendant of this tree
-        return true;
+ * check to see if the provided tree is already a child of this
+ * tree __or any of its sub trees__
+ */
+DFSelect.prototype.isDescendant = function(child) {
+   if(this.children.indexOf(child) !== -1) {
+      // `child` is an immediate child of this tree
+      return true;
+   } else {
+      for(var i = 0; i < this.children.length; i++) {
+         if(this.children[i].isDescendant(child)) {
+            // `child` is descendant of this tree
+            return true;
+         }
       }
-    }
-    return false;
-  }
+      return false;
+   }
 };
 
 /**
-  * remove an immediate child
-  */
-DFSelect.prototype.removeChild = function(child){
-  var index = this.children.indexOf(child);
-  if(index !== -1){
-    // remove the child
-    this.children.splice(index,1);
-  }else{
-    throw new Error("That node is not an immediate child of this tree");
-  }
+ * remove an immediate child
+ */
+DFSelect.prototype.removeChild = function(child) {
+   var index = this.children.indexOf(child);
+   if(index !== -1) {
+      // remove the child
+      this.children.splice(index, 1);
+   } else {
+      throw new Error("That node is not an immediate child of this tree");
+   }
 };
